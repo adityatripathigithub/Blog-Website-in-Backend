7cc6ba1d623ebf0fa21a83933faf20a914dc9cdb
adity tripathi <tripathiaditya163@gmail.com>
2024-07-12 23:54:45 +0530

    Edint Login.rjs And Register.ejs

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
+
+
 router.get('/register', function(req, res, next) {
   res.render('register');
 });
+
+
 
 module.exports = router;
